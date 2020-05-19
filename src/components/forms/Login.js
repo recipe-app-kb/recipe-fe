1237411661b7d1a7089597d1d64519c8db78ecab
Kerri-Ann <kaybee1jam@yahoo.com>
2020-05-19 13:51:30 -0400

    completed form validation

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Formik, withFormik, useFormik } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { loginUser } from '../../redux/actions/user-action';
 
 function Login(props) {
-	const { loginUser, loggedIn } = props;
+	const { loginUser, loggedIn, error } = props;
 
 	useEffect(() => {
 		if (loggedIn) {
@@ -15,12 +15,27 @@ function Login(props) {
 		}
 	}, [loggedIn])
 
+	const validate = values => {
+		const errors = {};
+
+		if (!values.username) {
+			errors.username = "Required";
+		}
+
+		if (!values.password) {
+			errors.password = "Required";
+		}
+
+		return errors;
+	}
+
+	// Handle form input
 	const formikLogin = useFormik({
 		initialValues: {
 			username: "",
 			password: ""
 		},
-
+		validate,
 		onSubmit: values => {
 			loginUser(values);
 		}
@@ -42,7 +57,9 @@ function Login(props) {
 									placeholder="Enter your username"
 									value={formikLogin.values.username}
 									onChange={formikLogin.handleChange}
+									onBlur={formikLogin.handleBlur}
 								/>
+								{(formikLogin.touched.username && formikLogin.errors.username) ? <div>Username is required</div> : null}
 							</FormGroup>
 							<FormGroup>
 								<Label for="password">Password:</Label>
@@ -53,9 +70,12 @@ function Login(props) {
 									placeholder="Enter your password"
 									value={formikLogin.values.password}
 									onChange={formikLogin.handleChange}
+									onBlur={formikLogin.handleBlur}
 								/>
+								{(formikLogin.touched.password && formikLogin.errors.password) ? <div>Password is required</div> : null}
 							</FormGroup>
-							<Button color="primary">Login</Button>
+							{error ? <div>Invalid username or password!</div> : null}
+							<Button color="primary" type="submit">Login</Button>
 							<Link to="/register">Don't have an account?</Link>
 						</Form>
 					</div>
@@ -68,7 +88,8 @@ function Login(props) {
 const mapStateToProps = (state) => {
 	return {
 		isLoggingIn: state.userReducer.isLoggingIn,
-		loggedIn: state.userReducer.loggedIn
+		loggedIn: state.userReducer.loggedIn,
+		error: state.userReducer.error
 	}
 }
 
