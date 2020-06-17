c8868131b01e4ca3e238bf4a6a8d931786f67101
Kerri-Ann <kaybee1jam@yahoo.com>
2020-06-17 19:11:37 -0400

    connected route to delete a step

--- src/redux/actions/step-actions.js
+++ src/redux/actions/step-actions.js
@@ -1,19 +1,35 @@
 import {
 	ADD_STEP_TO_RECIPE_START,
 	ADD_STEP_TO_RECIPE_SUCCESS,
-	ADD_STEP_TO_RECIPE_FAIL
+	ADD_STEP_TO_RECIPE_FAIL,
+	REMOVE_STEP_START,
+	REMOVE_STEP_SUCCESS,
+	REMOVE_STEP_FAIL,
 } from './types';
 import axios from 'axios';
 
 export const addStepToRecipe = (data) => dispatch => {
 	dispatch({ type: ADD_STEP_TO_RECIPE_START });
 
-	axios.post("http://localhost:5000/api/steps/", data)
+	axios.post("/steps/", data)
 		.then(res => {
 			console.log(res.data)
 			dispatch({ type: ADD_STEP_TO_RECIPE_SUCCESS })
 		})
 		.catch(err => {
 			dispatch({ type: ADD_STEP_TO_RECIPE_FAIL, payload: err })
+		})
+}
+
+export const removeStep = (id) => dispatch => {
+	dispatch({ type: REMOVE_STEP_START });
+
+	axios.delete(`/${id}`)
+		.then(res => {
+			console.log(res);
+			dispatch({ type: REMOVE_STEP_SUCCESS });
+		})
+		.catch(err => {
+			dispatch({ type: REMOVE_STEP_FAIL, payload: err });
 		})
 }
