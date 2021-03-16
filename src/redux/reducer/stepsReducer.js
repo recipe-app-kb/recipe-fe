307b93daa998f541311517970314275c78c7c001
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-03-16 18:54:26 -0400

    add state management for step removal

--- src/redux/reducer/stepsReducer.js
+++ src/redux/reducer/stepsReducer.js
@@ -1,12 +1,17 @@
 import {
 	ADD_STEP_TO_RECIPE_START,
 	ADD_STEP_TO_RECIPE_SUCCESS,
-	ADD_STEP_TO_RECIPE_FAIL
+	ADD_STEP_TO_RECIPE_FAIL,
+	REMOVE_STEP_START,
+	REMOVE_STEP_SUCCESS,
+	REMOVE_STEP_FAIL,
 } from '../actions/types';
 
 const initialState = {
 	isAddingStep: false,
 	addedStep: false,
+	isDeletingStep: false,
+	deletedStep: false,
 	error: null
 }
 
@@ -28,6 +33,24 @@ const stepsReducer = (state = initialState, action) => {
 			return {
 				isAddingStep: false,
 				addedStep: false,
+				error: action.payload
+			}
+		case REMOVE_STEP_START:
+			return {
+				isDeletingStep: true,
+				deletedStep: false,
+				error: null
+			}
+		case REMOVE_STEP_SUCCESS:
+			return {
+				isDeletingStep: false,
+				deletedStep: true,
+				error: null
+			}
+		case REMOVE_STEP_FAIL:
+			return {
+				isDeletingStep: false,
+				deletedStep: false,
 				error: action.payload
 			}
 		default:
