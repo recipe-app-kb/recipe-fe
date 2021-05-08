f267cf30537633a31b6e13ba156faaae31021a67
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-05-08 17:06:30 -0400

    fixed step reducer

--- src/redux/reducer/stepsReducer.js
+++ src/redux/reducer/stepsReducer.js
@@ -26,31 +26,35 @@ const stepsReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_STEPS_START:
 			return {
+				...state,
 				isFetchingSteps: true,
 				steps: state.steps,
 				error: null
 			}
 		case FETCH_STEPS_SUCCESS:
 			return {
+				...state,
 				isFetchingSteps: false,
 				steps: action.payload,
 				error: null
 			}
 		case FETCH_STEPS_FAIL:
 			return {
+				...state,
 				isFetchingSteps: false,
 				steps: [],
 				error: action.payload
 			}
 		case ADD_STEP_TO_RECIPE_START:
 			return {
+				...state,
 				isAddingStep: true,
 				isFetchingSteps: true,
-				steps: state.steps,
 				error: null
 			}
 		case ADD_STEP_TO_RECIPE_SUCCESS:
 			return {
+				...state,
 				isAddingStep: false,
 				isFetchingSteps: false,
 				steps: [...state.steps, action.payload],
@@ -58,44 +62,45 @@ const stepsReducer = (state = initialState, action) => {
 			}
 		case ADD_STEP_TO_RECIPE_FAIL:
 			return {
+				...state,
 				isAddingStep: false,
 				isFetchingSteps: false,
-				steps: state.steps,
 				error: action.payload
 			}
 		case REMOVE_STEP_START:
 			return {
+				...state,
 				isDeletingStep: true,
-				steps: state.steps,
 				error: null
 			}
 		case REMOVE_STEP_SUCCESS:
 			return {
+				...state,
 				isDeletingStep: false,
 				steps: state.steps.filter(step => step.id !== action.payload),
 				error: null
 			}
 		case REMOVE_STEP_FAIL:
 			return {
+				...state,
 				isDeletingStep: false,
-				steps: state.steps,
 				error: action.payload
 			}
 		case EDIT_STEPS_START:
 			return {
-				steps: state.steps,
+				...state,
 				updatedCount: null,
 				error: null
 			}
 		case EDIT_STEPS_SUCCESS:
 			return {
-				steps: state.steps,
+				...state,
 				updatedCount: action.payload,
 				error: null
 			}
 		case EDIT_STEPS_FAIL:
 			return {
-				steps: state.steps,
+				...state,
 				updatedCount: false,
 				error: action.payload
 			}
